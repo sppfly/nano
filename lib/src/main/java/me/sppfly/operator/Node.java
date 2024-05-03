b270f9ed03fabb42bf502d4188099f29d298b731
liuyi <liuyicn6@qq.com>
2024-05-03 15:53:56 +0200

    a

--- lib/src/main/java/me/sppfly/operator/Node.java
+++ lib/src/main/java/me/sppfly/operator/Node.java
@@ -1,23 +1,7 @@
 package me.sppfly.operator;
 
-public class Node extends Thread {
+public interface Node extends Active, Named {
 	
-	private Operator<?, ?> operator;
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		super.run();
-		while (operator.isEnabled()) {
-			operator.
-		}
-	}
-
-	@Override
-	public void start() {
-		super.start();
-		this.operator.enable();
-	}
 
 	
 
